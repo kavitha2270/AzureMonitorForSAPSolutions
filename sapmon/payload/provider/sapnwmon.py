6bbeedde6981ad051a0bed716a7a95a35ad0a470
rohan patil <ropatil@microsoft.com>
2020-10-22 07:44:53 +0000

    provider code update

--- sapmon/payload/provider/sapnwmon.py
+++ sapmon/payload/provider/sapnwmon.py
@@ -4,7 +4,7 @@ import json
 import logging
 import re
 import time
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, time
 
 # Payload modules
 from const import *
@@ -88,14 +88,33 @@ class SAPNWMonProviderInstance(ProviderInstance):
                 connection.call('RFCPING')
                 self.tracer.info("successfully called RFC ping.")
 
-                smon_result = self._call_sdf_get_smon_runs(connection)
+                # test call 
+                currentTimestampResult = self._call_bdl_get_central_timestamp(connection)
+                if currentTimestampResult is None:
+                    return False
+                currentDate, currentTime = self._process_bdl_get_central_timestamp_result(currentTimestampResult)
+                self.tracer.info("successfully retrieved timestamp from BDL_GET_CENTRAL_TIMESTAMP")
+
+                smon_result = self._call_sdf_get_smon_runs(connection, currentDate, currentDate)
                 if smon_result is None:
                     return False
                 guid = self._process_guid_using_smon_runs(smon_result)
                 self.tracer.info("successfully retrieved GUID from /SDF/GET_SMON_RUNS.")
 
-                smon_analysis_result = self._call_sdf_smon_analysis_read(connection, guid, datetime.now(), 0)
+                # TODO: if smon analysis does not returns results raise error.
+                # test if sdf/smon_analysis_run returns result for last minute.
+                startTime = (datetime.combine(date(year=1, month=1, day=1), currentTime) - timedelta(minutes=1)).time()
+                # for test
+                currentDate = date(2020, 10, 22)
+                startTime = time(0, 1)
+                currentTime = time(23, 59)
+                ##
+                smon_analysis_result = self._call_sdf_smon_analysis_read(connection, guid, currentDate, startTime, currentTime)
                 if smon_analysis_result is None:
+                    return False
+                processedResult = self._process_sdf_smon_analysis_read(smon_analysis_result)
+                if len(processedResult) == 0:
+                    self.tracer.error("RFC SDF/SMON_ANALYSIS_READ result did not return values. Check RFC setup.")
                     return False
         except Exception as e:
             self.tracer.error("Error occured while validating %s " % (e))
@@ -119,16 +138,51 @@ class SAPNWMonProviderInstance(ProviderInstance):
 
         return connection
 
-    def _call_sdf_get_smon_runs(self, connection: Connection):
-        from_date = datetime(1971, 5, 20).date()
-        to_date = datetime(2999, 12, 31).date()
+    def _call_bdl_get_central_timestamp(self, connection: Connection):
+        timestampResult = None
         try:
-            smon_result = connection.call('/SDF/SMON_GET_SMON_RUNS', FROM_DATE=from_date, TO_DATE=to_date)
+            timestampResult = connection.call('BDL_GET_CENTRAL_TIMESTAMP')
         except CommunicationError as e:
             self.tracer.error("Cannot establish connection with (%s) with hostname: %s " % (self.fullName, self.sapHostName))
             return None
         except Exception as e:
-            self.tracer.error("Error occured while establishing connection (%s) " % (e))
+            self.tracer.error("Error occured while calling rfc BDL_GET_CENTRAL_TIMESTAMP (%s) " % (e))
+            return None
+
+        return timestampResult
+
+    def _process_bdl_get_central_timestamp_result(self, result: Dict[str, str]):
+        if result is None:
+            raise ValueError("Invalid result received from BDL_GET_CENTRAL_TIMESTAMP")
+
+        currentDate: date = None
+        currentTime: time = None
+
+        if 'TAG' in result:
+            currentDate = datetime.strptime(result['TAG'], '%Y%m%d').date()
+        else:
+            raise ValueError("RFC BDL_GET_CENTRAL_TIMESTAMP result does not have TAG value.")
+
+        if 'UHRZEIT' in result:
+            currentTime = datetime.strptime(result['UHRZEIT'], '%H%M%S').time()
+        else:
+            raise ValueError("RFC BDL_GET_CENTRAL_TIMESTAMP result does not have TAG value.")
+
+        return currentDate, currentTime
+
+    def _call_sdf_get_smon_runs(self, connection: Connection, fromDate: date=None, toDate: date=None):
+        if fromDate is None:
+            fromDate = datetime(1971, 5, 20).date()
+
+        if toDate is None:
+            toDate = datetime(2999, 12, 31).date()
+        try:
+            smon_result = connection.call('/SDF/SMON_GET_SMON_RUNS', FROM_DATE=fromDate, TO_DATE=toDate)
+        except CommunicationError as e:
+            self.tracer.error("Cannot establish connection with (%s) with hostname: %s " % (self.fullName, self.sapHostName))
+            return None
+        except Exception as e:
+            self.tracer.error("Error occured while calling /SDF/SMON_GET_SMON_RUNS (%s) " % (e))
             return None
 
         return smon_result
@@ -143,26 +197,35 @@ class SAPNWMonProviderInstance(ProviderInstance):
         else:
             raise ValueError("SMON_RUNS value does not exist in /SDF/SMON_GET_SMON_RUNS return result.")
 
-    # TODO: edge case scenario related to datetime calculation.
-    def _call_sdf_smon_analysis_read(self, connection: Connection,  guid: str, lastRunTime: datetime, frequency: int):
-        # calculate next run time.
-        nextRunTime = lastRunTime + timedelta(seconds=frequency)
-
-        # RFC parameters.
-        datum = nextRunTime.date()
-        startTime = lastRunTime.time()
-        endTime = nextRunTime.time()
-
+    # call RFC SDF/SMON_ANALYSIS_RUN and return the result.
+    def _call_sdf_smon_analysis_read(self, connection: Connection, guid: str, currentDate: date, startTime: time, endTime: time):
+        result = None
         try:
-            result = connection.call('/SDF/SMON_ANALYSIS_READ', GUID=guid, DATUM=datum, START_TIME=startTime, END_TIME=endTime)
+            result = connection.call('/SDF/SMON_ANALYSIS_READ', GUID=guid, DATUM=currentDate, START_TIME=startTime, END_TIME=endTime)
         except CommunicationError as e:
             self.tracer.error("Cannot establish connection with (%s) with hostname: %s " % (self.fullName, self.sapHostName))
             return None
         except Exception as e:
-            self.tracer.error("Error occured while establishing connection (%s) " % (e))
+            self.tracer.error("Error occured while calling RFC /SDF/SMON_ANALYSIS_READ (%s) " % (e))
             return None
 
         return result
+
+    # return header information from sdf/smon_analysis_read
+    def _process_sdf_smon_analysis_read(self, result, filterList: List[str]=None):
+        if result is None:
+            raise ValueError("Invalid result received from SDF/SMON_ANALYSIS_READ")
+
+        processedResult = None
+        if 'HEADER' in result:
+            # create new dictionary with only values from filterList if filter dictionary exists.
+            processedResult = result['HEADER']
+            if filterList:
+                processedResult = { columnName: processedResult[columnName] for columnName in filterList }
+        else:
+            raise ValueError("SDF/SMON_ANALYSIS_READ result does not contain HEADER key.")
+
+        return processedResult
 
 ###########################
 # implement sapnwmon check.
