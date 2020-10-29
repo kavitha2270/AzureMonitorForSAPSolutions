b6b79243e5d8bbb0207445405d0264301f025d04
Jasneet Bhatti <JasneetBhatti@microsoft.com>
2020-10-29 19:53:06 +0000

    Added new action SWNC_GET_WORKLOAD_SNAPSHOT in SAPWMon Provider

--- sapmon/payload/provider/sapnwmon.py
+++ sapmon/payload/provider/sapnwmon.py
@@ -110,6 +110,11 @@ class SAPNWMonProviderInstance(ProviderInstance):
                 if len(processedResult) == 0:
                     self.tracer.error("RFC SDF/SMON_ANALYSIS_READ result did not return values. Check RFC setup.")
                     return False
+
+                swnc_result = self._call_swnc_get_workload_snapshot(connection, currentDate, currentDate, currentTime, currentTime)
+                if swnc_result is None:
+                    return False
+                self.tracer.info("successfully called SWNC_GET_WORKLOAD_SNAPSHOT.")
         except Exception as e:
             self.tracer.error("Error occured while validating %s " % (e))
             return False
@@ -227,23 +232,75 @@ class SAPNWMonProviderInstance(ProviderInstance):
 
         return processedResult
 
+    def _call_swnc_get_workload_snapshot(self, connection: Connection, fromDate: date=None, toDate: date=None, fromTime: time=None, toTime: time=None):
+        if fromDate is None:
+            fromDate = datetime(1971, 5, 20).date()
+
+        if toDate is None:
+            toDate = datetime(2999, 12, 31).date()
+        try:
+            swnc_result = connection.call('SWNC_GET_WORKLOAD_SNAPSHOT', READ_START_DATE=fromDate, READ_END_DATE=toDate, READ_START_TIME=fromTime, READ_END_TIME=toTime)
+        except CommunicationError as e:
+            self.tracer.error("Cannot establish connection with (%s) with hostname: %s " % (self.fullName, self.sapHostName))
+            return None
+        except Exception as e:
+            self.tracer.error("Error occured while calling SWNC_GET_WORKLOAD_SNAPSHOT (%s) " % (e))
+            return None
+
+        return swnc_result
+
+    def _process_swnc_get_workload_snapshot(self, result):
+        if result is None:
+            raise ValueError("Invalid result received from SWNC_GET_WORKLOAD_SNAPSHOT")
+
+        def GetSafe(dictionary, key):
+            if key not in dictionary:
+                raise ValueError("Result received from SWNC_GET_WORKLOAD_SNAPSHOT does not contain %s" % (key))
+            return dictionary[key]
+
+        records = [(record, {
+            "Number of Steps": GetSafe(record, 'COUNT'),
+            "Response Time": GetSafe(record, 'RESPTI'),
+            "CPU Time": GetSafe(record, 'CPUTI'),
+            "Total DB Time": GetSafe(record, 'READSEQTI') + GetSafe(record, 'CHNGTI') + GetSafe(record, 'READDIRTI'),
+            "STO3_RSPNSE": GetSafe(record, 'RESPTI') / GetSafe(record, 'COUNT'),
+            "STO3_CPUTIME": GetSafe(record, 'CPUTI') / GetSafe(record, 'RESPTI'),
+            "STO3_DB": (GetSafe(record, 'READSEQTI') + GetSafe(record, 'CHNGTI') + GetSafe(record, 'READDIRTI')) / GetSafe(record, 'RESPTI')
+        })
+        for record in GetSafe(result, 'TASKTIMES') if GetSafe(record, 'TASKTYPE') in (b'\xfe', b'\x01')]
+
+        processed_result = list()
+        for record, dictionary in records:
+            if (GetSafe(record, 'READDIRCNT') != 0):
+                dictionary['STO3_Avg_Dir_DB_Time'] = GetSafe(record, 'READDIRTI') / GetSafe(record, 'READDIRCNT')
+
+            if (GetSafe(record, 'READSEQTI') != 0):
+                dictionary['ST03_Avg_Seq_DB_Time'] = GetSafe(record, 'READSEQTI') / GetSafe(record, 'READSEQCNT')
+
+            if (GetSafe(record, 'CHNGCNT') != 0):
+                dictionary['ST03_Avg_chng_DB_Time'] = GetSafe(record, 'CHNGTI') / GetSafe(record, 'CHNGCNT')
+
+            processed_result.append(dictionary)
+
+        return processed_result
+
 ###########################
 # implement sapnwmon check.
 class SAPNWMonProviderCheck(ProviderCheck):
     lastResult = list()
-    lastRunTimeSDFSMON = None
 
     def __init__(self,
     provider: ProviderInstance,
     **kwargs
     ):
+        self.lastRunTime = None
         return super().__init__(provider, **kwargs)
 
     def _actionExecuteSDFSMON(self, columnList: List[str]):
         self.tracer.info("executing RFC SDF/SMON_ANALYSIS_RUN check")
         with self.providerInstance._establish_connection_to_sap() as connection:
             # get last run sap time.
-            lastRunTime = self.state.get('lastRunServer', None)
+            lastSapRunTime = self.state.get('lastRunServer', None)
 
             # read current time from SAP NetWeaver.
             timestampResult = self.providerInstance._call_bdl_get_central_timestamp(connection)
@@ -254,44 +311,73 @@ class SAPNWMonProviderCheck(ProviderCheck):
             guid = self.providerInstance._process_guid_using_smon_runs(guidResult)
 
             # based on last run and current time, calculate start and end time.
-            startDate, startTime, endTime = self.getNextRunTime(currentDate, currentTime, lastRunTime)
+            startDate, startTime, _, endTime = self.getNextRunTime(currentDate, currentTime, lastSapRunTime)
             smon_analysis_result = self.providerInstance._call_sdf_smon_analysis_read(connection, guid, startDate, startTime, currentTime)
             self.tracer.info("executed RFC SDF/SMON_ANALYSIS_READ with date: %s start time: %s to end time %s" % (startDate, startTime, endTime))
             smon_result = self.providerInstance._process_sdf_smon_analysis_read(smon_analysis_result, columnList)
 
             # only add to list, if results exist.
             if len(smon_result) != 0:
-                self.lastResult.extend(smon_result)
-            self.lastRunTimeSDFSMON = datetime.combine(startDate, endTime)
+                self.lastResult = smon_result
+            self.lastRunTime = datetime.combine(startDate, endTime)
 
         # Update internal state
         if not self.updateState():
             raise Exception("Failed to update state")
         self.tracer.info("successfully processed RFC SDF/SMON_ANALYSIS_READ result.")
 
-    def getNextRunTime(self, currentDate: date, currentTime: time, lastRunTime: datetime):
-        if lastRunTime is None:
+    def _actionExecuteSWNCGetWorkloadSnapshot(self):
+        self.tracer.info("executing RFC SWNC_GET_WORKLOAD_SNAPSHOT check")
+        with self.providerInstance._establish_connection_to_sap() as connection:
+            # get last run sap time.
+            lastSapRunTime = self.state.get('lastRunServer', None)
+
+            # read current time from SAP NetWeaver.
+            timestampResult = self.providerInstance._call_bdl_get_central_timestamp(connection)
+            currentDate, currentTime = self.providerInstance._process_bdl_get_central_timestamp_result(timestampResult)
+
+            # based on last run and current time, calculate start and end time.
+            startDate, startTime, endDate, endTime = self.getNextRunTime(currentDate, currentTime, lastSapRunTime)
+            swnc_get_workload_snapshot_result = self.providerInstance._call_swnc_get_workload_snapshot(connection, \
+                fromDate = startDate, toDate = endDate, fromTime = startTime, toTime = endTime)
+            self.tracer.info("executed RFC SWNC_GET_WORKLOAD_SNAPSHOT for window: %s start time: %s to %s end time: %s" % (startDate, startTime, endDate, endTime))
+            swnc_result = self.providerInstance._process_swnc_get_workload_snapshot(swnc_get_workload_snapshot_result)
+
+            # only add to list, if results exist.
+            if len(swnc_result) != 0:
+                self.lastResult = swnc_result
+            self.lastRunTime = datetime.combine(endDate, endTime)
+
+        # Update internal state
+        if not self.updateState():
+            raise Exception("Failed to update state")
+        self.tracer.info("successfully processed RFC SWNC_GET_WORKLOAD_SNAPSHOT result.")
+
+    def getNextRunTime(self, currentDate: date, currentTime: time, lastSapRunTime: datetime):
+        if lastSapRunTime is None:
             startDate = currentDate
+            endDate = currentDate
             endTime = currentTime
+            currentDateTime = datetime.combine(currentDate, currentTime)
             
             # set start time to current time minus frequency.
             startTime = None
-            endTimeSeconds = (endTime.hour * 3600) + (endTime.minute * 60) + endTime.second
+            endTimeSeconds = (currentDateTime - datetime.combine(currentDate, time(0, 0, 0))).total_seconds()
             if endTimeSeconds < self.frequencySecs:
                 startTime = time(0, 0, 0)
             else:
                 startTime = (datetime.combine(date(year=1, month=1, day=1), currentTime) - timedelta(seconds=self.frequencySecs)).time()
             
-            return startDate, startTime, endTime
+            return startDate, startTime, endDate, endTime
         else:
             # next start time is 1 sec after last server run time.
-            startDateTime = lastRunTime + timedelta(seconds=1)
+            startDateTime = lastSapRunTime + timedelta(seconds=1)
             endDateTime = datetime.combine(currentDate, currentTime)
 
             # if end date is moved to the next day, change end date to end on current date.
             if startDateTime.day != endDateTime.day:
                 endDateTime = datetime.combine(startDateTime.date(), time(23, 59, 59))
-            return startDateTime.date(), startDateTime.time(), endDateTime.time()
+            return startDateTime.date(), startDateTime.time(), endDateTime.date(), endDateTime.time()
 
     def generateJsonString(self) -> str:
         self.tracer.info("[%s] converting rfc result to json string." % self.fullName)
@@ -307,7 +393,6 @@ class SAPNWMonProviderCheck(ProviderCheck):
         lastRunLocal = datetime.utcnow()
         self.state['lastRunLocal'] = lastRunLocal
         # update last run server.
-        self.state['lastRunServer'] = self.lastRunTimeSDFSMON
+        self.state['lastRunServer'] = self.lastRunTime
         self.tracer.info("[%s] internal state successfully updated" % self.fullName)
         return True
-
