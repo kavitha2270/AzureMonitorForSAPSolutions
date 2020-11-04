a38ca277c06343a898cbe73f215a5c7b6eaab6d4
Syeda Persia Aziz <syaziz@microsoft.com>
2020-11-04 12:57:53 -0800

    payload version

--- sapmon/payload/const.py
+++ sapmon/payload/const.py
@@ -3,7 +3,7 @@ import logging
 import os
 
 # Version of the payload script
-PAYLOAD_VERSION = "2.4"
+PAYLOAD_VERSION = "2.5"
 
 # Default file/directory locations
 PATH_PAYLOAD       = os.path.dirname(os.path.realpath(__file__))
